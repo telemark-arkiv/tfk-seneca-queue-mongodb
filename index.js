2eb43f64288b90647b865e769e9c418b6f7c2aa1
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-10-17 11:01:06 +0200

    Initial commit

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
 'use strict'
 
-const addDocument = require('./lib/add-document')
-const getNextDocument = require('./get-next-document')
-const deleteDocument = require('./lib/delete-document')
+const mongojs = require('mongojs')
 
 module.exports = function (options) {
   const seneca = this
+  const dbQueue = mongojs(options.MONGODB_URI || 'localhost/queue')
+  const queue = dbQueue.collection(options.MONGODB_COLLECTION_NAME || 'queue')
+
+  const addDocument = function (args, callback) {
+    const payload = args.data
+
+    payload.timeStamp = new Date().getTime()
+
+    queue.save(payload, (error, document) => {
+      if (error) {
+        callback(error, null)
+      } else {
+        seneca.act({role: 'info', info: 'queue', msg: 'add', data: document})
+        callback(null, document)
+      }
+    })
+  }
+
+  const deleteDocument = function (args, callback) {
+    const queueId = mongojs.ObjectId(args.queueId)
+
+    queue.remove({'_id': queueId}, (error, document) => {
+      if (error) {
+        callback(error, null)
+      } else {
+        seneca.act({role: 'info', info: 'queue', msg: 'delete', data: document})
+        callback(null, document)
+      }
+    })
+  }
+
+  const getNextDocument = function (args, callback) {
+    queue.find({}).sort({timeStamp: 1}).limit(1, (error, document) => {
+      if (error) {
+        callback(error, null)
+      } else {
+        callback(null, document)
+      }
+    })
+  }
 
   seneca.add('role:queue, cmd:add', addDocument)
   seneca.add('role:queue, cmd:next', getNextDocument)
---
+++ test/modules/seneca-queue-test.js
@@ -0,0 +1,6 @@
+'use strict'
+
+const tap = require('tap')
+const senecaQueue = require('../../index')
+
+tap.ok(senecaQueue, 'tfk-seneca-queue-mongodb loads OK')
